9eb1f1be2ca46c1d277c28e2ade6da2e99d86153
onyeka mmakwe <notmaks@yahoo.com>
2019-12-07 22:22:37 +0100

    feat(implement category page)

--- src/components/organisms/SectionCard1/SectionCard1.jsx
+++ src/components/organisms/SectionCard1/SectionCard1.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { GridLoader } from 'react-spinners';
 import Title from '../../molecules/Title/Title';
 import Card from '../../molecules/Card/Card';
 
@@ -8,19 +9,31 @@ import './SectionCard1.scss';
 const PopularSection = ({ title, data }) => {
   return (
     <div>
-      <div className="section__title">
-        <Title title={title} />
-      </div>
-      <div className="section__grid">
-        {data.map(starship => (
-          <Card
-            key={starship.name}
-            text={starship.name}
-            model={starship.model}
-            cargo={starship.cargo_capacity}
-          />
-        ))}
-      </div>
+      {data === undefined ? (
+        <div className="loader">
+          <GridLoader size={50} color="#e3e3e3" />
+        </div>
+      ) : (
+        <>
+          <div className="section__title">
+            <Title title={title} />
+          </div>
+          <div className="section__grid">
+            {data.map(starship => (
+              <Card
+                key={starship.name}
+                text={starship.name}
+                model={starship.model}
+                cargo={starship.cargo_capacity}
+                climate={starship.climate}
+                population={starship.population}
+                birthYear={starship.birth_year}
+                gender={starship.gender}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
